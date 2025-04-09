dd8245924d08a898164326a7da5fade77a48605c
Milos <procaptain1029@gmail.com>
2025-04-09 13:39:34 +0200

    feat: all rotation

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { sqrt } from 'three/tsl';
 
 const manager = new THREE.LoadingManager();
 
@@ -22,8 +21,11 @@ scene.background = new THREE.Color( 0xa0a0a0 );
 const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 1 );
 hemiLight.position.set( 0, 20, 0 );
 scene.add( hemiLight );
-const dirLight = new THREE.DirectionalLight( 0xffffff, 5 );
-dirLight.position.set( 0, 20, 10 );
+const dirLight = new THREE.DirectionalLight( 0xffffff, 2 );
+const ambiantLight = new THREE.AmbientLight(0xffffff, 1);
+ambiantLight.position.set(0, -20, 0);
+scene.add( ambiantLight )
+dirLight.position.set( 0, 50, 10 );
 dirLight.castShadow = true;
 dirLight.shadow.camera.top = 1.8;
 dirLight.shadow.camera.bottom = - 1.0;
@@ -36,7 +38,7 @@ mesh.rotation.x = - Math.PI / 2;
 mesh.receiveShadow = true;
 scene.add( mesh );
 
-const grid = new THREE.GridHelper( 20, 20, 0x000000, 0x000000 );
+const grid = new THREE.GridHelper( 20, 20, 0x00000, 0x00000 );
 grid.material.opacity = 0.2;
 grid.material.transparent = true;
 scene.add( grid );
@@ -77,15 +79,10 @@ function loadAsset() {
         const model = gltf.scene;
         model.scale.set(1, 1, 1)
         model.position.set(0, 0.2, 0);
-        // console.log("model = ", model.children[0].children[1].children[0].children[4].children[0].children[1].children[0].children[0].children[0]);
-
         rotor1 = model.children[0].children[1].children[0].children[4].children[0].children[1].children[0].children[0].children[0];
         rotor2 = model.children[0].children[1].children[0].children[4].children[0].children[1].children[1].children[0].children[0];
         rotor3 = model.children[0].children[1].children[0].children[8].children[0].children[1].children[0].children[0].children[0];
         rotor4 =model.children[0].children[1].children[0].children[8].children[0].children[1].children[1].children[0].children[0];
-
-        console.log("rotor1 = ", rotor1)
-
         scene.add( model );
         loading.style.display = 'none';
     });
@@ -97,34 +94,46 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-let rotor1_x0 = 37 / 2;
-let rotor1_y0 = 49.15 / 2;
-let x00;
-let y00;
+let rotor1_x = 37 / 2;
+let rotor1_y = 49.15 / 2;
+let rotor2_x = 36.5 / 2;
+let rotor2_y = 49.1 / 2;
+let x1, x2, x3, x4;
+let y1, y2, y3, y4;
 
 function animate() {
-    if (rotor1) {
-        // rotor1.rotation.z = 0;
-        // rotor1.rotation.z = Math.PI;
-        // console.log("rotor1 = ", rotor1.rotation.z)
-        rotor1.rotation.z = index * 1.7;
-        x00 = calc(rotor1_x0, rotor1_y0, rotor1.rotation.z).x1;
-        y00 = calc(rotor1_x0, rotor1_y0, rotor1.rotation.z).y1;
-        // rotor1.position.set(-9.2, -38.56, 0)
-        // rotor1.position.set(rotor1_x0 * 2, -rotor1_y0 * 2, 0);
-        // rotor1.position.set(0, 0, 0);
-        rotor1.position.set(-x00, y00, 0);
-        // rotor1.position.set(36.7, -49.1, 0)
+    if (rotor1 && isPlay) {
+        const angle = index * 1.7;
+        rotor1.rotation.z = angle;
+        x1 = calc_pos(rotor1_x, rotor1_y, angle).x2;
+        y1 = calc_pos(rotor1_x, rotor1_y, angle).y2;
+        rotor1.position.set(-x1, y1, 0);
+
+        rotor2.rotation.z = angle;
+        x2 = calc_pos(-rotor2_x, rotor2_y, angle).x2;
+        y2 = calc_pos(-rotor2_x, rotor2_y, angle).y2;
+        rotor2.position.set(-x2, y2, 0);
+        
+        rotor3.rotation.z = angle;
+        x3 = calc_pos(rotor1_x, -rotor1_y, angle).x2;
+        y3 = calc_pos(rotor1_x, -rotor1_y, angle).y2;
+        rotor3.position.set(-x3, y3, 0);
+        
+        rotor4.rotation.z = angle;
+        x4 = calc_pos(-rotor2_x, -rotor2_y, angle).x2;
+        y4 = calc_pos(-rotor2_x, -rotor2_y, angle).y2;
+        rotor4.position.set(-x4, y4, 0);
+
         index ++;
     }
     renderer.render( scene, camera );
 }
 
-function calc(x0, y0, alpha) {
-    const r = Math.sqrt(x0 * x0 + y0 * y0);
-    const sita = Math.atan2(y0, x0);
+function calc_pos(x1, y1, alpha) {
+    const r = Math.sqrt(x1 * x1 + y1 * y1);
+    const sita = Math.atan2(y1, x1);
     const beta = sita - alpha;
-    const x1 = x0 - r * Math.cos(beta);
-    const y1 = y0 - r * Math.sin(beta);
-    return {x1, y1};
+    const x2 = x1 - r * Math.cos(beta);
+    const y2 = y1 - r * Math.sin(beta);
+    return {x2, y2};
 }
