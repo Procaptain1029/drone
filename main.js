1fa70372e12d2c8496e4f48c8d5d9ed80a1ca7f9
Milos <procaptain1029@gmail.com>
2025-04-09 12:37:19 +0200

    feat: rotation of fan

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { sqrt } from 'three/tsl';
 
 const manager = new THREE.LoadingManager();
 
 let camera, scene, renderer, loader;
+let rotor1, rotor2, rotor3, rotor4;
 let index = 0;
 let isPlay = false;
 
@@ -74,8 +76,16 @@ function loadAsset() {
     loader.load( '/models/drone.glb', function ( gltf ) {
         const model = gltf.scene;
         model.scale.set(1, 1, 1)
-        model.position.set(0, 0.2, 0)
-        console.log("model = ", model);
+        model.position.set(0, 0.2, 0);
+        // console.log("model = ", model.children[0].children[1].children[0].children[4].children[0].children[1].children[0].children[0].children[0]);
+
+        rotor1 = model.children[0].children[1].children[0].children[4].children[0].children[1].children[0].children[0].children[0];
+        rotor2 = model.children[0].children[1].children[0].children[4].children[0].children[1].children[1].children[0].children[0];
+        rotor3 = model.children[0].children[1].children[0].children[8].children[0].children[1].children[0].children[0].children[0];
+        rotor4 =model.children[0].children[1].children[0].children[8].children[0].children[1].children[1].children[0].children[0];
+
+        console.log("rotor1 = ", rotor1)
+
         scene.add( model );
         loading.style.display = 'none';
     });
@@ -87,9 +97,34 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
+let rotor1_x0 = 37 / 2;
+let rotor1_y0 = 49.15 / 2;
+let x00;
+let y00;
+
 function animate() {
-    if (isPlay) {
+    if (rotor1) {
+        // rotor1.rotation.z = 0;
+        // rotor1.rotation.z = Math.PI;
+        // console.log("rotor1 = ", rotor1.rotation.z)
+        rotor1.rotation.z = index * 1.7;
+        x00 = calc(rotor1_x0, rotor1_y0, rotor1.rotation.z).x1;
+        y00 = calc(rotor1_x0, rotor1_y0, rotor1.rotation.z).y1;
+        // rotor1.position.set(-9.2, -38.56, 0)
+        // rotor1.position.set(rotor1_x0 * 2, -rotor1_y0 * 2, 0);
+        // rotor1.position.set(0, 0, 0);
+        rotor1.position.set(-x00, y00, 0);
+        // rotor1.position.set(36.7, -49.1, 0)
         index ++;
     }
     renderer.render( scene, camera );
+}
+
+function calc(x0, y0, alpha) {
+    const r = Math.sqrt(x0 * x0 + y0 * y0);
+    const sita = Math.atan2(y0, x0);
+    const beta = sita - alpha;
+    const x1 = x0 - r * Math.cos(beta);
+    const y1 = y0 - r * Math.sin(beta);
+    return {x1, y1};
 }
